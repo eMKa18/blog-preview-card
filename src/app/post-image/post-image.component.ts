da6bfc5917c4978c6d8686b4ce08c3a337a2ed82
eMKa18 <1918102+eMKa18@users.noreply.github.com>
2024-07-10 15:08:10 +0200

    Adding post image component

---
+++ src/app/post-image/post-image.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-image',
+  standalone: true,
+  imports: [],
+  templateUrl: './post-image.component.html',
+  styleUrl: './post-image.component.css'
+})
+export class PostImageComponent {
+
+}
