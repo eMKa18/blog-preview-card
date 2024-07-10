ded50f5d68f565cc161b255d908d9f6fe4aa9ee3
eMKa18 <1918102+eMKa18@users.noreply.github.com>
2024-07-10 15:05:21 +0200

    Adding inital blog preview component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BlogPreviewComponent } from "./blog-preview/blog-preview.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BlogPreviewComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/blog-preview/blog-preview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'blog-preview',
+  standalone: true,
+  imports: [],
+  templateUrl: './blog-preview.component.html',
+  styleUrl: './blog-preview.component.css'
+})
+export class BlogPreviewComponent {
+
+}
